aba0635f359dcfcb7db9fcaf2e530376b3bac3c4
Stephen <stephen.yin@outlook.com>
2019-11-24 13:56:31 +0800

    Updates

---
+++ precache-manifest.6e36d6546954072be30090ffa71150e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df56d8fbcc25cbc7a60f0f7df5b922be",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "43343c5b778236369dda",
+    "url": "/game-of-life/static/css/main.4190adfe.chunk.css"
+  },
+  {
+    "revision": "9c6720ea859123808b76",
+    "url": "/game-of-life/static/js/2.a3a67c59.chunk.js"
+  },
+  {
+    "revision": "43343c5b778236369dda",
+    "url": "/game-of-life/static/js/main.f55b7e80.chunk.js"
+  },
+  {
+    "revision": "6c337d97cb58ee876942",
+    "url": "/game-of-life/static/js/runtime-main.51be29ed.js"
+  }
+]);
